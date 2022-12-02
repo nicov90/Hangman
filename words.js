0d752006c7d865a220e4f576551abf7e8f33a249
Nicolás Valdez <nico_v99@hotmail.com>
2022-12-02 11:56:20 -0300

    everything working

--- words.js
+++ words.js
@@ -1,13 +1,13 @@
 const wordInput = document.querySelector("#word-input");
 const buttonSave = document.querySelector("#button-save");
-const words = ['Futbol','Egoista','Conexion','Corazon','Hormiga','Almohada','Exhausto','Deporte','Extintor','Herreria','Flexible','Zapallo','Manzana','Arbol','Economia','Puerta','Parasito','Hospital','Basilica',
+const words = (JSON.parse(localStorage.getItem("newWordsList"))).words || ['Futbol','Egoista','Conexion','Corazon','Hormiga','Almohada','Exhausto','Deporte','Extintor','Herreria','Flexible','Zapallo','Manzana','Arbol','Economia','Puerta','Parasito','Hospital','Basilica',
                 'Montaña','Anhelar','Taxista','Exhibido','Guitarra','Saxofon','Borracho','Proyecto','Extremo','Hexagono','Pieza','Hipoteca','Honduras','Prohibir','Casa','Tablero','Vehiculo','Consola','Adhesivo','Lazo'];
 const wordReady = [];
 const errorMessage = document.querySelector(".error-message");
 let input = "";
 
 function randomWord(){
-    let chosenWord = words[Math.floor(Math.random()*(words.length-1))]
+    let chosenWord = words[Math.floor(Math.random()*(words.length-1))+1]
     console.log(chosenWord);
     for(let i=0;i<chosenWord.length;i++){
         let letter = (chosenWord.charAt(i)).toUpperCase();
@@ -15,7 +15,6 @@ function randomWord(){
     }
 }
 buttonSave.addEventListener("click",(e)=>{
-    e.preventDefault();
     let error = "";
     const regexAdd = /^[A-Za-z\u00F1\u00D1]+$/;
     if(regexAdd.test(wordInput.value)){
@@ -24,12 +23,16 @@ buttonSave.addEventListener("click",(e)=>{
         if(input.length >= 4 && input.length <= 8){
             error = "";
             words.push(input);
+            localStorage.setItem("newWordsList",JSON.stringify({words}));
+
             console.log(words);
             }
             else{
+                e.preventDefault();
                 error = "La palabra debe tener al menos 4 letras";
             }
     }else{
+        e.preventDefault();
         error = "La palabra no debe contener números, ni caracteres especiales y debe tener entre 4 y 8 letras.";
     }
     errorMessage.innerHTML = error;
